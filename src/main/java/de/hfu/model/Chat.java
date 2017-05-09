9bb573c8d81caed4fce1a94d76bfb239022082a0
Christian Plotzky <christian@plotzky.net>
2017-05-09 21:20:09 +0200

    various firebase functions

--- src/main/java/de/hfu/model/Chat.java
+++ src/main/java/de/hfu/model/Chat.java
@@ -7,41 +7,74 @@ import java.util.List;
  */
 public class Chat {
 
-    private String name;
-    private long timestamp;
-    private List<User> participants;
-    private User creator;
+	private String name;
+	private String id;
+	private long timestamp;
+	private List<String> participants;
+	private String creator;
+	private List<Message> messages;
 
+	public String getName() {
+		return name;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public long getTimestamp() {
+		return timestamp;
+	}
 
-    public long getTimestamp() {
-        return timestamp;
-    }
+	public void setTimestamp(long timestamp) {
+		this.timestamp = timestamp;
+	}
 
-    public void setTimestamp(long timestamp) {
-        this.timestamp = timestamp;
-    }
+	public List<String> getParticipants() {
+		return participants;
+	}
 
-    public List<User> getParticipants() {
-        return participants;
-    }
+	public void setParticipants(List<String> participants) {
+		this.participants = participants;
+	}
 
-    public void setParticipants(List<User> participants) {
-        this.participants = participants;
-    }
+	public String getCreator() {
+		return creator;
+	}
 
-    public User getCreator() {
-        return creator;
-    }
+	public void setCreator(String creator) {
+		this.creator = creator;
+	}
 
-    public void setCreator(User creator) {
-        this.creator = creator;
-    }
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public void copyChat(Chat chat) {
+		this.creator = chat.creator;
+		this.id = chat.id;
+		this.name = chat.name;
+		this.participants = chat.participants;
+		this.timestamp = chat.timestamp;
+		this.messages = chat.messages;
+	}
+
+	public List<Message> getMessages() {
+		return messages;
+	}
+
+	public void setMessages(List<Message> messages) {
+		this.messages = messages;
+	}
+
+	@Override
+	public String toString() {
+		return "Chat [name=" + name + ", id=" + id + ", timestamp=" + timestamp + ", participants=" + participants
+				+ ", creator=" + creator + ", messages=" + messages + "]";
+	}
+	
 }
